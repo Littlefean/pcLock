93153f5afc98f811892f8450a662f9685b2cbe4e
littlefean <2028140990@qq.com>
2023-06-13 14:20:13 +0800

    早期的修改

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from tkinter import *
+
 from stage_clock import StageClock
 from observer import Observer
 from auto_action import Captor, MouseAction, show_blue_screen
@@ -7,7 +9,7 @@ def main():
     mouse = MouseAction()
     cap = Captor("result", "D:/桌面/grabRes")
 
-    clock = StageClock(10, 20)
+    clock = StageClock(60, 600)
 
     clock.status1_target = mouse.lock
     clock.status2_target = show_blue_screen
@@ -34,6 +36,12 @@ def main():
     obs.start()
 
     clock.start()
+
+    root = Tk()
+    root.title("hello world")
+    root.geometry('250x100')  # 是x 不是*
+    root.resizable(width=False, height=True)  # 宽不可变, 高可变,默认为True
+    root.mainloop()
 
 
 if __name__ == "__main__":
